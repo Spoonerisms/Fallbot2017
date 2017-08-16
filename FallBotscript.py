7e8aeba9ddbe3ac5d5ee4e5973118f31e9866ee3
Spoonerisms <kirstenaobrien@gmail.com>
2017-08-15 21:47:00 -0700

    Add files via upload

--- FallBotscript.py
+++ FallBotscript.py
@@ -9,3 +9,10 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+def countdown(n) :
+while n > 0:
+print (n)
+n = n - 1
+if n ==0:
+print('There are (n) days until fall.')
+#countdown(50)

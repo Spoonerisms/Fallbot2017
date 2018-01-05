9c4c0c7ee6f46d378cde8892170b507fba766b56
Sam Stelle <samuelstelle@gmail.com>
2018-01-05 11:35:24 -0500

    cleaned it up a bit - will need to rework timing

--- FallBotscript.py
+++ FallBotscript.py
@@ -9,37 +9,41 @@ from time import sleep
 from datetime import datetime, timedelta
 
 
-### Commented out since I don't have this info
+"""
+Commented out since I don't have this info
+"""
 
 # from FallBotTwitterauth import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 
-### setting this as the constant for TAPI
+"""
+setting this as the constant for TAPI
+"""
 # TAPI = tweepy.API(auth)
 
-
+### 
 PHRASE_BOOK = [
     'Fall starts in %s',
     'Leaves start changing color in %s',
-    'Time till fall %s',
-    "FML till %s",
+    'Time till fall: %s',
+    "FML for the next %s",
+    "No pumpin spice lattes for the next %s",
+    "%s until I start my seasonal depression all over again",
+    "My favorite season starts in %s",
 ]
-
-
 FALL_START = datetime(2018, 9, 22)
-LAST_RUN = datetime.today()
+
 
 def get_time_till_fall():
     now = datetime.now()
     d = FALL_START - now
 
-    time_map = {}
-    d = str(d).split('.')[0]
+    # string formatting to make it look good
+    d = str(d).split('.')[0] # remove miliseconds
     s = d.split(':')
     d = "%s hours, %s minutes and %s seconds" % (s[0], s[1], s[2])
-
     return d
 
 
@@ -47,7 +51,10 @@ def randomize_day():
     seconds_in_day = 86400
     time_to_run = int(random.random() * seconds_in_day)
     
-    ### leave commented out until you know it works since this will halt the program for 0 - 24 hours
+    """
+    leave commented out until you know it works since this will halt the program for 0 - 24 hours
+    """
+
     # sleep(time_to_run)
 
 def post_to_twitter():
@@ -60,5 +67,7 @@ def run():
     post_to_twitter()
 
 if __name__ == "__main__":
-    # We assume this function will run at midnight each day then randomly post during the day\
+    """
+    We assume this function will run at midnight each day then randomly post during the day.
+    """
     run()
